6b2458ea471497395d36ea7b1eb1f5e03f2439d9
malburo <tongquocbao0605@gmail.com>
2020-05-21 19:46:17 +0700

    validate form login

--- src/components/Form/Login/FormLogin.js
+++ src/components/Form/Login/FormLogin.js
@@ -1,30 +1,49 @@
 import React from "react";
 import "./FormLogin.css";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  InputGroup,
+  FormFeedback,
+} from "reactstrap";
 const FormLogin = (props) => {
+  const { onChangeHandler, onSubmit, errors, isComplete } = props;
   return (
     <div className="wrap">
       <div className="FormLogin">
         <h1 className="FormLogin__logo">Malburo</h1>
-        <Form className="FormLogin__input">
+        <Form className="FormLogin__input" onSubmit={onSubmit} method="POST">
           <FormGroup>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="Tên người dùng hoặc email"
-              className="FormLogin__email"
-            />
+            <InputGroup>
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Tên người dùng hoặc email"
+                className="FormLogin__email"
+                onChange={onChangeHandler}
+                invalid={errors.username}
+              />
+              <FormFeedback invalid={true}>{errors.username}</FormFeedback>
+            </InputGroup>
           </FormGroup>
           <FormGroup>
-            <Input
-              type="password"
-              name="password"
-              id="examplePassword"
-              placeholder="Mật khẩu"
-              className="FormLogin__password"
-            />
+            <InputGroup>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="Mật khẩu"
+                className="FormLogin__password"
+                onChange={onChangeHandler}
+                invalid={errors.password}
+              />
+              <FormFeedback invalid={true}>{errors.password}</FormFeedback>
+            </InputGroup>
           </FormGroup>
+          {isComplete && <p>Đăng Nhập thành công</p>}
           <Button color="primary" block className="FormLogin__btn">
             Đăng nhập
           </Button>
