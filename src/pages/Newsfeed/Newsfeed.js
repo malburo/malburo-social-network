752be3da4b1e9601dcdc4a369bab1e546f9ae2f0
malburo <tongquocbao0605@gmail.com>
2020-05-23 11:20:24 +0700

    check accessToken

--- src/pages/Newsfeed/Newsfeed.js
+++ src/pages/Newsfeed/Newsfeed.js
@@ -2,19 +2,36 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import PostCard from "../../components/Card/PostCard";
 import { Container, Row, Col } from "reactstrap";
-const NewsfeedPage = (props) => {
-  return (
-    <React.Fragment>
-      <Header />
-      <Container style={{ paddingTop: 100 }}>
-        <Row>
-          <Col>
-            <PostCard />
-          </Col>
-        </Row>
-      </Container>
-    </React.Fragment>
-  );
-};
+import { Redirect } from "react-router-dom";
+class NewsfeedPage extends React.Component {
+  constructor(props) {
+    super(props);
+    let isLogin = true;
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken === null) {
+      isLogin = false;
+    }
+    this.state = {
+      isLogin,
+    };
+  }
+  render() {
+    if (this.state.isLogin === false) {
+      return <Redirect to={`/accounts/login`} />;
+    }
+    return (
+      <React.Fragment>
+        <Header />
+        <Container style={{ paddingTop: 100 }}>
+          <Row>
+            <Col>
+              <PostCard />
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
 
 export default NewsfeedPage;
